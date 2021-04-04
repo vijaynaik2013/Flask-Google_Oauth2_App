5001951f7f32494a9434028bac3dee108b5034ee
vijaynaik2013 <35876652+vijaynaik2013@users.noreply.github.com>
2021-04-05 00:35:04 +0530

    Delete config22.py

--- config22.py
+++
@@ -1,6 +0,0 @@
-import os
-
-#GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENTID')
-#GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
-GOOGLE_CLIENT_ID = 'GOOGLE_CLIENTID' # please create one under API & Services with your google account
-GOOGLE_CLIENT_SECRET = 'GOOGLE_CLIENT_SECRET' # please create one under API & Services with your google account
